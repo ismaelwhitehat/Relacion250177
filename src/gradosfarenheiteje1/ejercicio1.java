76ce961c05672e5c30a93ca6b02b2185bb6eeb19
admincenec <admincenec@TEMP503>
2017-01-25 13:32:54 +0100

    ejercicio 1

--- src/gradosfarenheiteje1/ejercicio1.java
+++ src/gradosfarenheiteje1/ejercicio1.java
@@ -1,9 +1,28 @@
 package gradosfarenheiteje1;
 
+import java.util.Scanner;
+
 public class ejercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		float gradosF = farenheit();
+		System.out.println(gradosF);
+	    System.out.println(); 
+	    
+	}
+
+	public static float farenheit() {
+		float gradosF;
+		float gradosC;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Introduce grados centígrados: ");
+		gradosC = teclado.nextFloat();
+		gradosF =32 + (9 * gradosC / 5);
+		System.out.println(gradosC +" ºC = " + gradosF + " ºF");
+
+		return gradosF;
 
 	}
 
